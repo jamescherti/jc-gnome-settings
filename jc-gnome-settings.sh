f94bdf2e3c770d5f9d0e8d5c1f20a75b02664960
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-03-07 10:46:26 -0500

    Remove gtk-application-prefer-dark-theme

--- jc-gnome-settings.sh
+++ jc-gnome-settings.sh
@@ -226,8 +226,6 @@ main() {
   gset org.gnome.desktop.interface clock-show-date true
   gset org.gnome.desktop.interface clock-show-weekday true
   gset org.gnome.desktop.interface clock-format 12h
-
-  gset org.gnome.desktop.interface gtk-application-prefer-dark-theme true
 }
 
 init
